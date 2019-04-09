74772e721756892e329c4d635dfb6d51c6fb0a50
mlvovna <ts.m.lvovna@gmail.com>
2019-04-09 16:30:52 +0600

    дз к 6 уроку

--- ДЗ6/6_2.py
+++ ДЗ6/6_2.py
@@ -1,19 +1,33 @@
-from pympler import asizeof
 import sys
-
 from random import random
-n = 10
-m = [0] * n
-even = []
-for i in range(n)
-	m[i] = int(random() * 10 ) + 10
-	if m[i] % 2 == 0:
-		even.append(i)
-print(m)
-print('Индексы четных элементов массива: ',even)
 
+n = 15
+arr = [0]+n
+for i in range(n)
+	arr[i] = int(random()*100)
+	print(arr[i],end='')
+print()
+	
+mn = 0
+mx = 0
+for i in range(n):
+	if arr[i] < arr[mn]:
+		mn = i
+	elif arr[i] > arr[mx]:
+		mx = i
+print('arr[%d}=%d arr[%d}=%d' % (mn+1, arr[mn], mx+1, arr[mx])
+b = arr[mn]
+arr[mn] = arr[mx]
+arr[mx] = b
+	
+for i in range(15)
+	print(arr[i],end='')
+print()
+	
 print(sys.getsizeof(n))
-print(sys.getsizeof(m))
+print(sys.getsizeof(arr))
+print(sys.getsizeof(mn))
+print(sys.getsizeof(mx))
 
 #Python 3.7.2
 #ОС 64-разрядная
